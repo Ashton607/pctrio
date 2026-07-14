c144fc47080a48c3352a81af943efdcd419d98e4
Ashton607 <portmanashton4@gmail.com>
2026-07-14 19:17:16 +0200

    Added: navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/PCTRIO_LOGO.png'
 
 const Navbar = () => {
   return (
     <nav>
-      
+    <h2>PC<span>TRIO</span>
+    </h2>
+    <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>About Us</li>
+        <li>Our Work</li>
+        <li>Contact</li>
+    </ul>
+    <span className='whatsapp'>Direct Message</span>
     </nav>
   )
 }
