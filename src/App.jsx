a36dc0769f17ef185bb4e7f85f421a3605212ebf
Ashton607 <portmanashton4@gmail.com>
2026-07-14 18:54:12 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/nav/Navbar'
+
+function App() {
+
+  return (
+    <Router>
+    <Navbar/>
+    <Routes>
+      
+    </Routes>
+    </Router>
+  )
+}
+
+export default App
