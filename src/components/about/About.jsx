5f28edc6eb82c601d4977307800adcd0347608bb
Ashton607 <portmanashton4@gmail.com>
2026-07-15 18:39:47 +0200

    Added: about jsx file

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='About'>
+    <div className="about-content">
+    
+    </div>
+    </div>
+  )
+}
+
+export default About
