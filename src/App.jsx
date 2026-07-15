09d70b14f1ec144a86501c631a6b5626fc2c5745
Ashton607 <portmanashton4@gmail.com>
2026-07-15 19:18:25 +0200

    Installed: react scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/nav/Navbar'
 import Hero from './components/hero/Hero'
 import Services from './components/services/Services'
+import About from './components/about/About'
 
 function App() {
-
   return (
     <Router>
-    <Navbar/>
-    <Hero/>
-    <Routes>
-      <Route path="/" element={<Services />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Services />
+            </>
+          }
+        />
+        <Route path="about" element={<About />} />
+      </Routes>
     </Router>
   )
 }
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,9 +4,16 @@ import './About.css'
 const About = () => {
   return (
     <div className='About'>
+
     <div className="about-content">
-    
+    <span className="about-badge">about us</span>
+    <h1 className="about-title">
+        <span className="line1">Three shops,</span>
+        <span className="line2">one address.</span>
+        <span className="line3">one address.</span>
+    </h1>
     </div>
+    
     </div>
   )
 }
